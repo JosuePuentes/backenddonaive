3d36e60558f2735f739e4b5cda21af25a2a8d840
stalin carruyo <carruyoadarmezstalin@gmail.com>
2025-05-23 11:10:12 -0400

    configuracion de .env

--- app/db/mongo.py
+++ app/db/mongo.py
@@ -2,7 +2,7 @@ from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorCollection
 from app.core.config import MONGO_URI, DATABASE_NAME
 import certifi
 
-client = AsyncIOMotorClient("mongodb+srv://rapifarma:30780142@cluster0.9nirn5t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",tlsCAFile=certifi.where())
+client = AsyncIOMotorClient(MONGO_URI,tlsCAFile=certifi.where())
 db = client["RAPIFARMA"]
 
 def get_collection(nombre: str) -> AsyncIOMotorCollection:
