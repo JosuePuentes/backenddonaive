ca746cd601460c871a185657adc43530d6fe5656
ispulzar <stan.stalinprogramado@gmail.com>
2025-07-31 16:22:29 -0400

    actualizacion metas

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.routes_example import router as example_router
-from app.routes import auth
+from app.routes import auth, metas
 from app.routes.pagoscpp import router as pagoscpp_router
 
 
@@ -20,3 +20,4 @@ app.add_middleware(
 app.include_router(example_router, prefix="/api/v1")
 app.include_router(auth.router)
 app.include_router(pagoscpp_router)
+app.include_router(metas.router, tags=["metas"])
